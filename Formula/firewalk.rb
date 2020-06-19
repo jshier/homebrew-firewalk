64c73536dbef0e2f1f5272d4fd42b400ad7f5868
Jon Shier <jon@jonshier.com>
2020-06-18 20:33:35 -0400

    0.0.2 release.

--- Formula/firewalk.rb
+++ Formula/firewalk.rb
@@ -2,7 +2,7 @@ class Firewalk < Formula
   desc "Firewalk HTTP Test Server"
   homepage "https://github.com/jshier/firewalk"
 
-  url "https://github.com/jshier/Firewalk/releases/download/0.0.1/firewalk.tar.gz", :using => :curl
+  url "https://github.com/jshier/Firewalk/releases/download/0.0.2/firewalk.tar.gz", :using => :curl
 
   def install
     bin.install "firewalk"
